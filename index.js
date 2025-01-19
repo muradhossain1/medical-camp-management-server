1863ba38001e446134d5cf69557e2d468cf0636c
muradhossain1 <muradssq12@gmail.com>
2025-01-19 19:19:24 +0600

    Create jwt token and create middlewere verifytoken or verifyAdmin

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors())
@@ -27,21 +28,85 @@ async function run() {
 
         const userCollection = client.db('medicalDB').collection('users')
 
+        // jwt releted api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ token })
+        })
+
+        //middlewere
+        const verifyToken = (req, res, next) => {
+            // console.log('inside verifyToken', req.headers.authorization)
+            if (!req.headers.authorization) {
+                return res.status(401).send({ massage: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ massage: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        };
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email};
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if(!isAdmin){
+                return res.status(403).send({ massage: 'forbidden  access' })
+            }
+            next();
+        }
+
+
         // users releted api 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result)
+        })
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ massage: 'forbidden  access' })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            let admin = false;
+            if (user) {
+                admin = user?.role === "admin";
+            }
+            res.send({ admin })
         })
         app.post('/users', async (req, res) => {
             const user = req.body;
 
             const query = { email: user.email }
             const existingUser = await userCollection.findOne(query)
-            if(existingUser){
-                return res.send({message: 'user already exists '})
+            if (existingUser) {
+                return res.send({ message: 'user already exists ' })
             }
 
             const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+        app.patch('/users/admin/:id',verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+        app.delete('/users/:id',verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
         // Send a ping to confirm a successful connection
